af9972744643b36f23309679841481e2b6f1d468
Victor <victorgrinan@gmail.com>
2025-05-04 17:31:28 +0300

    adding extra tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
-import getSheetData from './service/sheetService'
-import { useEffect, useState } from 'react'
+import './App.css';
+import getSheetData from './service/sheetService';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState("home");
+  const [tab, setTab] = useState("table");
+ 
   
   const date = new Date()
   const today = date.getDay();
@@ -37,23 +39,33 @@ function App() {
   const handlePayment = (page) => {
     setPage(page)
   }
+  const handleTabs = (tab) => {
+    setTab(tab)
+  }
   return (
     <> 
       {page=="home" &&
         <img src={`https://lh3.googleusercontent.com/d/${import.meta.env.VITE_IMG_ID}`} width={200}/>
       }
       {console.log(`https://lh3.googleusercontent.com/d/${import.meta.env.VITE_IMG_ID}`)}
-      <h1 className="read-the-docs">
+      {page!=="concept" &&       
+        <h1 className="read-the-docs">
         Gymmy some love
-      </h1>
+        </h1>
+        }
       {page=="home" &&<h2>Today {date.toDateString()}</h2>}
+      {page=="home" && <button onClick={()=>handlePayment("concept")}>Main program concept</button>}
       {page=="home" && 
       <section className="Today" style={{width:"100%", color:"white", textAlign:"left"}}>
         <ul>
-        {data && data.map((d, i) => d ? <li key={i}>{d}</li> : null)}
+        {data && 
+          data.map((d, i) => 
+            i === 0 ? null : d ? <li key={i}> {d} </li> : null)
+        }
         </ul>
         <button onClick={()=>handlePayment("payment")}>Pay</button>
       </section>}
+      
        {page === "payment" && <>
         <div style={{display:"flex", alignItems:"center", gap:"10px", flexWrap:"wrap", justifyContent:"center"}}>
           <script async
@@ -89,6 +101,64 @@ function App() {
         </div> 
         <button onClick={()=>handlePayment("home")}>Cancel</button>
         </>
+        }
+        {page === "concept" && 
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center", gap:"10px", flexWrap:"wrap", justifyContent:"center"}}>
+          <h2>Main program concept</h2>
+
+          {tab === "table" &&
+          
+          <div>
+              <table border="1" cellpadding="8" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th>Weight (% of 1RM) <button onClick={()=>handleTabs("1rm")}>?</button> </th>
+                    <th>Repetitions</th>
+                    <th>Sets</th>
+                    <th>Goal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>60–80%</td>
+                    <td>6–12</td>
+                    <td>3–4</td>
+                    <td>Mass Gain</td>
+                  </tr>
+                  <tr>
+                    <td>50–70%</td>
+                    <td>12–20+</td>
+                    <td>2–4</td>
+                    <td>Weight Loss / Endurance</td>
+                  </tr>
+                </tbody>
+              </table>
+
+            <button onClick={()=>handlePayment("home")}>Back to home</button>
+          </div>
+          
+          }
+
+        {tab === "1rm" &&
+          
+          <div>
+            <button onClick={()=>handleTabs("table")}>Table</button>
+
+            <p>Weight (% of 1RM) means the load used in a set as a percentage of your One-Rep Max (1RM) — the maximum weight you can lift once for a given exercise.</p>
+
+            <p>Example:</p>
+            <p>If your 1RM for bench press is 100 kg:</p>
+
+              <ul>
+                <li>85% of 1RM = 85 kg</li>
+                <li>70% of 1RM = 70 kg</li>
+              </ul>
+            <p>If you reach the max repetitions in all sets you need to recalculate your 1MR or increase the sets</p>
+            <button onClick={()=>handlePayment("home")}>Back to home</button>
+          </div>
+          
+          }
+        </div>
         }
     </>
   )
